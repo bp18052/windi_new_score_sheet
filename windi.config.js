7ade07cd2e01c41ee1a475b06c332eb8f851c2c5
bp18052 <bp18052@shibaura-it.ac.jp>
2022-03-03 16:35:18 +0900

    new branch

--- windi.config.js
+++ windi.config.js
@@ -11,5 +11,16 @@ export default defineConfig({
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    require('@windicss/animations')({
+      settings: {
+        animatedSpeed: 1000,
+        heartBeatSpeed: 1000,
+        hingeSpeed: 2000,
+        bounceInSpeed: 750,
+        bounceOutSpeed: 750,
+        animationDelaySpeed: 1000
+      },
+    }),
+  ],
 });

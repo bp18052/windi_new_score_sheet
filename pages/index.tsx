459b545a696552e9c9accdcb2efd2a4c7358be9c
bp18052 <bp18052@shibaura-it.ac.jp>
2022-03-07 12:18:08 +0900

    refact

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,54 +1,18 @@
 import Footer from 'components/footer'
 import SignButton from 'components/SignInButton'
 import type { NextPage } from 'next'
-import styles from '../styles/Home.module.css'
-
+import Image from 'next/image'
+import homeImage from '../public/HomeImage.jpg'
 const Home: NextPage = () => {
   return (
-    <div className={styles.container}>
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.tsx</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className={styles.card}
-          >
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-        <SignButton name="Twitter"/>
-        <div className='visible font-milka font-xl font-normal not-italic'>test font</div>
-      </main>
-      <Footer />
+    <div className=' bg-gray-600'>
+      <div className='block'>
+        <Image src={homeImage} alt="Picture of the author" className='relative z-0'/>
+        <figcaption>
+          <h1 className='itc animate-animated animate-fadeInUpBig z-1 mt-2'>Let&apos;s record <br />your score</h1>
+        </figcaption>
+      </div>
+      <span className=' bg-transparent block h-32'></span>
     </div>
   )
 }
--- windi.config.js
+++ windi.config.js
@@ -10,10 +10,17 @@ export default defineConfig({
     exclude: ['node_modules', '.git', '.next'],
   },
   theme: {
+    zIndex: {
+      first: 10,
+      second: 20,
+    },
     extend: {
       fontFamily: {
-        sans:['itc-avant-garde-gothic-pro','sans-serif'],
-        milka:['milka']
+        itc:['itc avant garde gothic pro'],
+        milka:['milka'],
+        milkaBrittle:['milka brittle'],
+        aryadouble:['arya double'],
+        aryatriple:['arya triple']
       },
       fontWeight: {
         'hairline': 100,
@@ -28,6 +35,9 @@ export default defineConfig({
         'extra-bold': 800,
         'black': 900,
       },
+      backgroundImage: theme => ({
+        'top-img': "url('public/HomeImage.jpg')"
+      })
     },
     fontSize: {
       'xs': '.75rem',
@@ -43,8 +53,31 @@ export default defineConfig({
       '6xl': '4rem',
       '7xl': '5rem',
     },
+    filter: {
+      none: 'none',
+      grayscale: 'grayscale(1)',
+      invert: 'invert(1)',
+      sepia: 'sepia(1)',
+    },
+    backdropFilter: {
+      none: 'none',
+      blur: 'blur(20px)',
+    },
+  },
+  shortcuts:{
+    'itc': 'visible font-itc font-normal not-italic',
+    'milka': 'visible font-milka font-normal not-italic',
+    'milka-brittle': 'visible font-milkaBrittle font-normal not-italic',
+    'aryadouble': 'visible font-aryadouble font-normal not-italic',
+    'aryatriple': 'visible font-aryatriple font-normal not-italic',
+  },
+  variants: {
+    filter: ['responsive'],
+    backdropFilter: ['responsive'],
   },
   plugins: [
+    require('windicss/plugin/aspect-ratio'),
+    require('windicss/plugin/filters'),
     require('@windicss/plugin-question-mark'),
     require('@windicss/plugin-animations')({
       settings: {

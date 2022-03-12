9b62c59ee095b814feab9fcad5ac9895e0dc2d6a
bp18052 <bp18052@shibaura-it.ac.jp>
2022-03-12 14:51:58 +0900

    change login layout

--- pages/LoginPage.tsx
+++ pages/LoginPage.tsx
@@ -1,13 +1,17 @@
 import Header from 'components/header'
 import type { NextPage } from 'next'
+import Image from 'next/image';
+import LoginImage from '../public/LoginImage.jpg'
+import { Auth, Typography, Button } from '@supabase/ui'
 
 const loginForm :NextPage = () =>{
     return(
         <div className=' bg-gray-800'>
             <Header />
-            <div>
-                
+            <div className=' inline-block '>
+
             </div>
+            <Image src={LoginImage} alt="archery target sheet" className=' inline-block'/>
         </div>
     )
 }

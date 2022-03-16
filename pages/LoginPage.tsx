015845ca0c7117371419a0f4e2ebae41c1e03da0
bp18052 <bp18052@shibaura-it.ac.jp>
2022-03-16 21:49:17 +0900

    complete sighnInpage layout

--- pages/LoginPage.tsx
+++ pages/LoginPage.tsx
@@ -1,7 +1,7 @@
 import Header from 'components/header'
 import type { NextPage } from 'next'
 import Image from 'next/image';
-import LoginImage from '../public/LoginImage.jpg'
+import LoginImage from '../public/SignInBackImg.jpg'
 import { Auth, Typography, Button } from '@supabase/ui'
 import supabase from '../utils/supabaseClient'
 
@@ -9,17 +9,16 @@ const loginForm :NextPage = () =>{
     return(
         <div>
             <Header />
-            <div className=' flex bg-gray-800 content-center justify-center'>
-                <div style={{width:'50vh', height:'100vh'}}>
+            <Image src={LoginImage} alt="archery target sheet" layout="fill" objectFit='cover'/>
+            <div className='flex'>
+                <div className=' z-2 bg-gray-800 h-screen w-50vw flex content-center justify-center'>
                     <div className='card w-96 bg-gray-300 shadow-xl'>
                         <div className='card-body text-center'>
                             <h1>Sign In</h1>
                         </div>
                     </div>
                 </div>
-                <div style={{width: '50vh',height: '100vh'}}>
-                    <Image src={LoginImage} alt="archery target sheet" layout="fill" objectFit='contain' objectPosition={'right center'}/>
-                </div>
+                <span className=' bg-transparent  h-screen w-50vw block'/>
             </div>
         </div>
     )

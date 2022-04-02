0246eb321707afa273b9c0bff8b29b5f7c66622b
bp18052 <bp18052@shibaura-it.ac.jp>
2022-04-02 19:24:00 +0900

    create catch api

--- components/MailForm.tsx
+++ components/MailForm.tsx
@@ -3,11 +3,6 @@ import { useForm, SubmitHandler, NestedValue } from 'react-hook-form';
 import { ErrorMessage } from '@hookform/error-message';
 import { string } from 'yup';
 
-type FormInput = {
-  mail: string;
-  pass: string;
-};
-
 const mailForm: VFC = () => {
 
   //hookFormのESlintエラーは無視
--- pages/api/catchSupabase.ts
+++ pages/api/catchSupabase.ts
@@ -1,2 +1,8 @@
 import { NextApiRequest, NextApiResponse } from 'next';
 import Supabase from '../../utils/supabaseClient';
+import mailForm from '../../components/MailForm';
+
+const getSupabaseAPI = async( req:NextApiRequest, res:NextApiResponse) => {
+
+}
+const signSupabase = (formData:FormInput) => {}
